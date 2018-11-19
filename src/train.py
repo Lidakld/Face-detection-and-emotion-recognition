29dbf533af689e8b0be60a6423144304c90bcd46
matthewxfz <matthewxfz@gmail.com>
2018-11-18 23:04:38 -0600

    debug train

--- src/train.py
+++ src/train.py
@@ -9,13 +9,20 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 import tensorflow as tf
 import os
-import src.fer2013_data as dataset_fer
+import fer2013_data as dataset_fer
 
 tf.app.flags.DEFINE_string('tf_data',
                            os.path.abspath('../data/fer2013/tfrecords'),
                            'Folder containing images.')
+
+tf.app.flags.DEFINE_string('save_path',
+                           os.path.abspath('../data/fer2013/train'),
+                           'Folder containing model results')
+
+
 FLAGS = tf.app.flags.FLAGS
 
 tf.logging.set_verbosity(tf.logging.INFO)
@@ -109,35 +116,59 @@ def train_op(model, labels):
     return loss, eval_metric_ops, input_tensor
 
 
-def train(epoch_iteration=10, batch_size = 30, angle_range = 15):
+def train(epoch_iteration=1000, batch_size = 8, angle_range = 15):
     tfrecords_filenames = os.path.join(FLAGS.tf_data, 'train-00000.tfrecord')
-    images, labels = dataset_fer.read_and_decode(tfrecords_filenames, epoch_iteration,
+    print("Read dataset in %s" % tfrecords_filenames)
+    images, labels = dataset_fer.read_and_decode(tfrecords_filenames, 1,
                    batch_size=batch_size,
                    num_threads=8,
                    max_angle=angle_range)
 
-
+    print("perpare trian op")
     model = cnn_model_fn(images)
-    loss, accuracy, input_tensor = train_op(model, labels)
+    loss, metrics, input_tensor = train_op(model, labels)
 
+    print("get optimizer")
     global_step = tf.Variable(0)
     learning_rate = tf.train.exponential_decay(0.1, global_step, 300, 0.99, staircase=True)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-
+    
+    print("start")
     with tf.Session() as sess:
+        sess.run(tf.local_variables_initializer())
         sess.run(tf.global_variables_initializer())
-        step = 0
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        print("start epoch")
         for epoch in range(epoch_iteration):
-                image, label = sess.run([images, labels])
-                _, loss, acc = sess.run([optimizer, loss, accuracy], feed_dict={input_tensor['image']:image,
+                try:
+                    image, label = sess.run([images, labels])
+                except:
+                    images, labels = dataset_fer.read_and_decode(tfrecords_filenames, 1,
+                        batch_size=batch_size,
+                        num_threads=8,
+                        max_angle=angle_range)
+                    image, label = sess.run([images, labels])
+                    continue
+
+                tf.logging.info("Label types %s" % str(np.unique(label)))
+                _, loss_v, acc = sess.run([optimizer, loss, metrics['accuracy']], feed_dict={input_tensor['image']:image,
                                                                                 input_tensor['label']:label})
-                if step % 1 == 0:
+                if epoch % 100 == 0:
                     acc_v = 0
                     print("In epoch %d, loss: %.3f, accuracy: %.3f, validation accuracy: %.3f" % (
-                    epoch, loss, acc, acc_v))
+                    epoch, loss_v, acc[0], 0))
         saver = tf.train.Saver()
         saver_path = saver.save(sess, SAVE_PATH)
         print('Finished!')
+        # Stop the threads
+        coord.request_stop()
+
+        # Wait for threads to stop
+        coord.join(threads)
+        sess.close()
 
 
 if __name__ == "__main__":
